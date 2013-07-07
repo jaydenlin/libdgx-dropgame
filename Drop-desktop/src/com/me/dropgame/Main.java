39190485c3d64f1b427c7ed80e64b73ef32d6b4e
jayden <jayden@180-176-26-71.dynamic.kbronet.com.tw>
2013-07-07 20:20:40 +0800

    first commit

---
+++ Drop-desktop/src/com/me/dropgame/Main.java
@@ -0,0 +1,16 @@
+package com.me.dropgame;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "Drop";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new dropgame(), cfg);
+	}
+}
